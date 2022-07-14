4510c41a044703425e83fdc50a0bce65602595c0
Mikhail V. Kuzmich <kmv85@mail.ru>
2022-07-14 14:46:15 +0300

    Algorithmization SIMPLE ARRAYS tasks 1-10 are done

--- Basics/src/Algorithmization/Algorithmization.java
+++ Basics/src/Algorithmization/Algorithmization.java
@@ -1,17 +1,13 @@
 package Algorithmization;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class Algorithmization {
 
     public static void main(String[] args) {
-        double[] arr = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19}; //35
+        int[] arr = {10, 2, 11, 6, 6, 6, 11, 1, 11, 3, 19, 3, 30};
         int number = 10;
-        System.out.println(getItemSumIfIndexPrime(arr));
-        ;
-
+        replaceEverySecondItemOnNull(arr);
     }
 
     /*
@@ -95,6 +91,70 @@ public class Algorithmization {
             }
         }
         return result;
+    }
+
+    //в массиве действиетльных чисел найти max а1 + а2n, a2 + a(2n - 1)...(Задание 7)
+    public static double getMaxSumOppositeItems(double[] array) {
+        double max = Double.MIN_VALUE;
+        for (int i = 0; i < array.length / 2; i++) {
+            double itemsSum = array[i] + array[array.length - 1 - i];
+            if (itemsSum > max) {
+                max = itemsSum;
+            }
+        }
+        return max;
+    }
+
+    //Из заданного массива целых чисел удалить элементы, равные min. Получить новый массив (Задание 8)
+    public static int[] getArrayFromGivenWithoutMinItem(int[] array) {
+        int min = Integer.MAX_VALUE;
+        int countMinItem = 1;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == min) {
+                countMinItem++;
+            }
+            if (array[i] < min) {
+                min = array[i];
+            }
+        }
+        int[] resultArray = new int[array.length - countMinItem];
+        int indexResultArray = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] != min) {
+                resultArray[indexResultArray++] = array[i];
+            }
+        }
+        return resultArray;
+    }
+
+    //Найти наиболее часто встречающееся число в целочисленном массиве(минимальное, если таких несколько) (Задание 9)
+    public static int findFrequentItem(int[] array) {
+        Arrays.sort(array);
+        int maxFrequency = 0;
+        int resultItem = 0;
+        int itemFrequency = 0;
+        for (int i = 0; i < array.length - 1; i++) {
+            if (array[i + 1] == array[i]) {
+                itemFrequency++;
+                if (itemFrequency > maxFrequency) {
+                    maxFrequency = itemFrequency;
+                    resultItem = array[i];
+                } else if (itemFrequency == maxFrequency) {
+                    resultItem = Math.min(array[i], resultItem);
+                }
+            } else {
+                itemFrequency = 0;
+            }
+        }
+        return resultItem;
+    }
+    //Заменить каждый второй элемент в целочисленном массиве на ноль (без дополнительного массива) (Задание 10)
+    public static void replaceEverySecondItemOnNull(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            if(i % 2 != 0) {
+                array[i] = 0;
+            }
+        }
     }
 
 }
