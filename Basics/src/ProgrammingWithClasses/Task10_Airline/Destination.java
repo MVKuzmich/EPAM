03d21f7562b355431c3f4df2837e7704af464707
Mikhail V. Kuzmich <kmv85@mail.ru>
2022-08-04 14:37:56 +0300

    Programming with classes task 10

---
+++ Basics/src/ProgrammingWithClasses/Task10_Airline/Destination.java
@@ -0,0 +1,5 @@
+package ProgrammingWithClasses.Task10_Airline;
+
+public enum Destination {
+    MOSCOW, WARSAW, LONDON, PARIS, BERLIN
+}
---
+++ Basics/src/ProgrammingWithClasses/Task10_Airline/PlaneType.java
@@ -0,0 +1,5 @@
+package ProgrammingWithClasses.Task10_Airline;
+
+public enum PlaneType {
+    AIRBUS, BOEING, EMBRAER
+}
