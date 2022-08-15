375864f7e946ceea6dee4dd9e7a3ff6503b2bbbb
Mikhail V. Kuzmich <kmv85@mail.ru>
2022-08-15 14:44:15 +0300

    Programming with classes - COMPOSITION - Task 2 Car - some changes

--- Basics/src/ProgrammingWithClasses/AggregationAndComposition/Task2_Car/CarTest.java
+++ Basics/src/ProgrammingWithClasses/AggregationAndComposition/Task2_Car/CarTest.java
@@ -3,26 +3,31 @@ package ProgrammingWithClasses.AggregationAndComposition.Task2_Car;
 public class CarTest {
 
     public static void main(String[] args) {
+        Car car = null;
+        try {
+            Engine engine = new Engine(200, 0.1, "Audi");
+            Wheel[] wheels = {
+                    new Wheel(15, "A"),
+                    new Wheel(15, "A"),
+                    new Wheel(15, "A"),
+                    new Wheel(15, "A")
+            };
+            car = new Car("Audi", engine, wheels, 100);
+            System.out.printf("Car model is %s\n", car.getCarModel());
+            car.refuel(90);
+            System.out.println(car);
 
-        Engine engine = new Engine(200, 0.1, "Audi");
-        Wheel[] wheels = {
-                new Wheel(15, "A"),
-                new Wheel(15, "A"),
-                new Wheel(15, "A"),
-                new Wheel(15, "A")
-        };
-        Car car = new Car("Audi", engine, wheels, 100);
-
-        car.refuel(90);
-        System.out.println(car);
-
-        car.go(50);
-        System.out.println(car);
-
+            car.go(50);
+            System.out.println(car);
+            car.go(1000);
+        } catch (IllegalArgumentException ex) {
+            System.out.println(ex.getMessage());
+        }
         Wheel wheelIn = new Wheel(15, "B");
-        car.changeWheel(WheelLocation.FRONT_LEFT, wheelIn);
-        System.out.println(car);
+        if (car != null) {
+            car.changeWheel(WheelLocation.FRONT_LEFT, wheelIn);
+            System.out.println(car);
+        }
     }
-
 
 }
